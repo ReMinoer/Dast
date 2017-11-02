69360bc8b6922be7fedfe05517c715bde5bbb1bf
Rémi Morillon <rmorillon@freneticexperience.com>
2017-11-02 22:38:58 +0100

    First implementation of Dash media output for HTML and Markdown

---
+++ Media/Html/Dast.Media.Html.Dash/DashConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Dast.Inputs.Dash;
+using Dast.Media.Contracts.Html;
+using Dast.Outputs.Html;
+
+namespace Dast.Media.Html.Dash
+{
+    public class DashConverter : HtmlMediaOutputBase
+    {
+        private readonly DashInput _dashInput;
+        private readonly FragmentedHtmlOutput _fragmentedHtmlOutput;
+        private IDictionary<HtmlFragment, string> _fragments;
+        
+        public override string DisplayName => "Dash document";
+        public override MediaType Type => MediaType.Visual;
+        public override IEnumerable<FileExtension> FileExtensions { get { yield return Dast.FileExtensions.Text.Dash; } }
+
+        public DashConverter()
+        {
+            _dashInput = new DashInput();
+            _fragmentedHtmlOutput = new FragmentedHtmlOutput();
+        }
+
+        public override string Convert(string extension, string content, bool inline)
+        {
+            var htmlFragments = new []
+            {
+                HtmlFragment.Body,
+                HtmlFragment.Notes
+            };
+
+            _fragments = _fragmentedHtmlOutput.Convert(_dashInput.Convert(content), htmlFragments);
+            return $"<figure>{Environment.NewLine}{_fragments[HtmlFragment.Body]}{Environment.NewLine}{_fragments[HtmlFragment.Notes]}{Environment.NewLine}</figure>";
+        }
+    }
+}
---
+++ Media/Markdown/Dast.Media.Markdown.Dash/DashConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Dast.Inputs.Dash;
+using Dast.Media.Contracts.Markdown;
+using Dast.Outputs.GitHubMarkdown;
+
+namespace Dast.Media.Markdown.Dash
+{
+    public class DashConverter : IMarkdownMediaOutput
+    {
+        private readonly DashInput _dashInput;
+        private readonly FragmentedGitHubMarkdownOutput _fragmentedGitHubMarkdownOutput;
+        private IDictionary<GithubMarkdownFragment, string> _fragments;
+        
+        public string DisplayName => "Dash document";
+        public MediaType Type => MediaType.Visual;
+        public IEnumerable<FileExtension> FileExtensions { get { yield return Dast.FileExtensions.Text.Dash; } }
+
+        public DashConverter()
+        {
+            _dashInput = new DashInput();
+            _fragmentedGitHubMarkdownOutput = new FragmentedGitHubMarkdownOutput();
+        }
+
+        public string Convert(string extension, string content, bool inline)
+        {
+            var githubMarkdownFragments = new []
+            {
+                GithubMarkdownFragment.Body,
+                GithubMarkdownFragment.Notes
+            };
+
+            _fragments = _fragmentedGitHubMarkdownOutput.Convert(_dashInput.Convert(content), githubMarkdownFragments);
+            return $"{_fragments[GithubMarkdownFragment.Body]}{Environment.NewLine}{_fragments[GithubMarkdownFragment.Notes]}";
+        }
+    }
+}
